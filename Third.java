caca8e0d5b002d6ca4cd154f85387468ebcab596
sinhayani <36432595+sinhayani@users.noreply.github.com>
2024-08-23 17:45:35 +0530

    Third Assignment

---
+++ Third.java
@@ -0,0 +1,22 @@
+/*
+ *You are given a string letters made of N English letters. Count the number of different letters that appear
+in both uppercase and lowercase where all lowercase occurrences of the given letter appear before any
+uppercase occurrence.
+For example, for letters = "aaAbcCABBc" the answer is 2. The condition is met for letters ‘a’ and ‘b’, but
+not for ‘c’.
+Write a function:
+class Solution { public int solution(String letters); }
+that, given a string letters, returns the number of different letters fulfilling the conditions above.
+Examples:
+1. Given letters = "aaAbcCABBc", the function should return 2, as explained above.
+2. Given letters = "xyzXYZabcABC", the function should return 6.
+3. Given letters = "ABCabcAefG", the function should return 0.
+Write an efficient algorithm for the following assumptions:
+N is an integer within the range [1..100,000];
+string letters is made only of letters (a−z and/or A−Z).
+ */
+
+public class Third {
+    
+    
+}
