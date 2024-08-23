517a60c12da0db43e3d664054458a0d14639005b
sinhayani <36432595+sinhayani@users.noreply.github.com>
2024-08-23 17:46:06 +0530

    Fourth assignment

---
+++ Fourth.java
@@ -0,0 +1,12 @@
+public class Forth {
+    public static void main(String[] args) {
+        
+        String examples[] = {"Sunday", "Monday", "Tuesday", "Wednesday"};
+        
+        System.out.println(examples[-3:-1]);
+    
+}
+}
+
+
+//Output: Syntax Error
